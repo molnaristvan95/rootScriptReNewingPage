d85785568d1435294c995695faa6152720a97a84
Molnár István <33826380+molnaristvan95@users.noreply.github.com>
2020-11-29 23:48:17 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,5 +1,33 @@
-function s(){
-alert('works');
+console.clear();
+
+function reOpenWin(time, path) {
+  var way = window.location.href,
+      faster = time - 1,
+      slower = time + 2;
+  if (way === path) {
+    window.setTimeout(abort, (faster * 1000));
+    window.setTimeout(openNewWindow(path), (time * 1000));
+    window.setTimeout(closePage, (slower * 1000));
+  } else {
+    console.error('%cErre az oldalra nem terjed ki a script.','color:yellow; font-size:24px;');
+  }
+}
+
+function abort() {
+  document.title = 'Lejárt oldal !!';
   return;
 }
-s();
+
+function openNewWindow(url) {
+  window.open(url, '_blank');
+  return;
+}
+
+function closePage() {
+  //return window.close();
+  window.location.href = '/closekiosk';
+  return;
+}
+
+//reOpenWin(10, 'file:///D:/Slack/local.html'); ←Local PATH from PC folder
+//reOpenWin(10, 'https://jsbin.com/jecison'); ←URL from the network
