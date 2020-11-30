6ce7defe393509d14ce3ff621bfe50e95d1ee8d6
Molnár István <33826380+molnaristvan95@users.noreply.github.com>
2020-11-30 01:09:35 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,43 +3,29 @@ console.clear();
 function reOpenWin(time, path) {
   var way = window.location.href,
       faster = time - 1,
-      slower = time + 2;
+      slower = time + 1;
   if (way === path) {
-    window.setTimeout(abort, (faster * 1000));
-    window.setTimeout(function(){
-      openNewWindow(path), (time * 1000)
-    });
-    window.setTimeout(closePage, (slower * 1000));
+    window.setTimeout(function() {
+      document.title = 'Lejárt oldal !!';
+    }, (faster * 1000));
+    
+    window.setTimeout(function() {
+      window.open(path, '_blank');
+    }, (time * 1000));
+    
+    window.setTimeout(function() {
+      document.body.innerHTML = 'A munkamenet lejárt.';
+      document.body.style.background = 'black';
+      document.body.style.color = 'yellow';
+      document.body.style.fontSize = '32px';
+      document.body.style.display = 'flex';
+      document.body.style.alignItems = 'center';
+      document.body.style.justifyContent = 'center';
+      document.body.style.height = '95vh';
+    }, (slower * 1000));
   } else {
     console.error('%cErre az oldalra nem terjed ki a script.','color:yellow; font-size:24px;');
   }
-}
-
-function abort() {
-  document.title = 'Lejárt oldal !!';
-  return;
-}
-
-function openNewWindow(url) {
-  window.open(url, '_blank');
-}
-
-function closePage() {
-  document.body.innerHTML = 'A munkamenet lejárt.';
-  document.body.style.background = 'black';
-  document.body.style.color = 'yellow';
-  document.body.style.fontSize = '32px';
-  document.body.style.display = 'flex';
-  document.body.style.alignItems = 'center';
-  document.body.style.justifyContent = 'center';
-  document.body.style.height = '95vh';
-  document.body.addEventListener('mousemove', halt);
-  document.body.addEventListener('click', halt);
-  return window.close();
-}
-
-function halt() {
-  return window.close();
 }
 
 //reOpenWin(10, 'file:///D:/Slack/local.html'); ←Local PATH from PC folder
